80ec1a2b8bd0001f4471397931e68daec5699906
sigmakawasaki69 <denisevdokimov0209@gmail.com>
2026-05-05 22:25:47 +0300

    fix start bot

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -2,9 +2,6 @@
 # 1. Apply database migrations (CRITICAL for fixing 500 error)
 #python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-# 2. Start bots in the background
-python bots/telegram_bot.py &
-python bots/discord_bot.py &
 
 # 3. Start Django with Gunicorn
 gunicorn core.wsgi --bind 0.0.0.0:${PORT:-8000}
